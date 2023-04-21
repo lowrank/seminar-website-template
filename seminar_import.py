3192ac919f235ed3a9cc0997a4878610c66cef98
Yimin Zhong <yzz0225@auburn.edu>
2023-04-21 13:40:23 -0500

    init commit

---
+++ seminar_import.py
@@ -0,0 +1,70 @@
+import string
+import pandas as pd
+import sys
+import datetime 
+
+def extract_data(url):
+    url_for_pandas = url.replace("/edit?usp=sharing", "/export?format=xlsx")
+    df = pd.read_excel(url_for_pandas)
+    return df
+
+# url = "https://docs.google.com/spreadsheets/d/1cFzxm3_-cEb2RLEbAkrJbO6QnqXs0OXK/edit?usp=sharing&ouid=113340153287953812217&rtpof=true&sd=true"
+# url_for_pandas = url.replace("/edit?usp=sharing", "/export?format=xlsx")
+# df = pd.read_excel(url_for_pandas)
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 3:
+        print("Usage: python3 seminar_import.py <year> <semester> google_sheet_url")
+        exit(1)
+    print("Generating yml files for Hugo site...")
+    print("Year: ", sys.argv[1])
+    print("Semester: ", sys.argv[2])
+    print("Google Sheet URL: ", sys.argv[3])
+
+    df = extract_data(sys.argv[3])
+    
+    items_yml = open("data/items_"+ str(sys.argv[1]) + "_" + str(sys.argv[2]) + ".yml", "w")
+    spotlights_yml = open("data/spotlights_"+ str(sys.argv[1]) + "_" + str(sys.argv[2]) + ".yml", "w")
+
+    
+    items_yml.write("title : \"Schedule\"\nid: \"schedule-"+str(sys.argv[1])+"-"+str(sys.argv[2])+"\"\ncontent: |\n "+str(sys.argv[2])+" "+str(sys.argv[1])+"\nstyle: \"style2 medium onscroll-fade-in\"\nitems:\n") 
+
+    spotlights_yml.write("spotlights:\n")
+
+    for index, row in df.iterrows():
+    
+        name     = row['Name']
+        website  = row['Website']
+        school   = row['School']
+        date     = row['Date']
+        time_loc = row['Time and Location']
+        title    = row['Title']
+        abstract = row['Abstract']
+
+        # convert date string 
+        date = datetime.datetime.strptime(str(date), '%Y-%m-%d %H:%M:%S').strftime('%b %d')
+
+        items_yml.write(" - name: " + "\"" + str(name) + "\"\n")
+        items_yml.write("   website: " +  str(website) + "\n")
+        items_yml.write("   school: " + "\"" + str(school) + "\"\n")
+        items_yml.write("   date: " + "\"" + str(date) + " at " + str(time_loc) + "\"\n")
+        items_yml.write("   link: " + "\"" + "#" + name.replace(" ", "") +"-" + str(sys.argv[1])+"-"+str(sys.argv[2]) + "\"\n")
+        items_yml.write("   content: " + "\'" + str(title) + "\'\n")
+        items_yml.write("   abstract: " +   str(abstract) + "\n")
+        items_yml.write("   style: " + "\"solid style2 major fa-people-line\"\n\n\n")
+
+
+        if index % 2 == 0:
+            orient = "right"
+        else:
+            orient = "left"
+
+        spotlights_yml.write("  - style: " + "\"style2 " + "orient-"+orient + " content-align-left" + " image-position-center" + " onscroll-image-fade-in\"\n")
+        spotlights_yml.write("    id: " + "\"" + name.replace(" ", "") +"-" + str(sys.argv[1])+"-"+str(sys.argv[2]) + "\"\n")
+        spotlights_yml.write("    title: " + "\"" + str(name) + "\"\n")
+        spotlights_yml.write("    content: |\n" + "      " + str(title) + "\n")
+        spotlights_yml.write("    abstract: " + str(abstract) + "\n")
+        spotlights_yml.write("    image: " + "\"images/" + name.replace(" ", "") + ".jpg" + "\"\n\n\n")
+    
+    items_yml.close()
+    spotlights_yml.close()
